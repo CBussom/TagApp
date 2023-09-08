0c3ced767e108031cc5de5b286e956e758c73b5d
CBussom <52250345+CBussom@users.noreply.github.com>
2023-09-08 13:46:54 -0400

    Initial commit

---
+++ app/src/main/java/com/example/tagapp/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.tagapp.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "History"
+    }
+    val text: LiveData<String> = _text
+}
